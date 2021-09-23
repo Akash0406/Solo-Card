e9bef3156d2e08237876e07ba227a539049e53cb
Akash Kumar Singh <67006219+StarX0406@users.noreply.github.com>
2021-09-23 23:42:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => {
+    item.addEventListener('click', () => {
+        removeActive();
+        item.classList.add('active');
+    })
+})
+
+function removeActive(){
+    items.forEach((item) => {
+            item.classList.remove('active');
+        })
+}
